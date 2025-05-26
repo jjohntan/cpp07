b8d5fc1c32b009c658b61e098534a85b21fbee11
Jek John Tan <jetan@u91z02s01.42kl.edu.my>
2025-05-26 14:02:35 +0800

    implement max

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -20,7 +20,7 @@ int main( void ) {
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
@@ -28,7 +28,7 @@ int main( void ) {
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
 	return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -31,4 +31,10 @@ T min(T &a, T &b)
 	return (a < b) ? a : b;
 }
 
+template <typename T>
+T max(T &a, T &b)
+{
+	return (a > b) ? a : b;
+}
+
 #endif
