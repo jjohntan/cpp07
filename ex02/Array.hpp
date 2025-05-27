2c729c7822e23946001b23818807ac60d649da87
Jek John Tan <jetan@u91z02s02.42kl.edu.my>
2025-05-27 18:01:09 +0800

    implement ocf for Array class template

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 16:48:52 by jetan             #+#    #+#             */
-/*   Updated: 2025/05/26 20:41:41 by jetan            ###   ########.fr       */
+/*   Updated: 2025/05/27 14:33:30 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,48 @@ template <typename T>
 class Array
 {
 	private:
-		T arr;
+		T *arr;
 		unsigned int _size;
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(const Array &other);
-		Array &operator=(const Array &other);
-		~Array();
+		Array()
+		{
+			arr = NULL;
+			_size = 0;
+		}
+		Array(unsigned int n): _size(n)
+		{
+			arr = new T[n];
+		}
+		Array(const Array &other)
+		{
+			*this = other;
+		}
+		Array &operator=(const Array &other)
+		{
+			if (this != other)
+			{
+				this->arr = other.arr;
+				if (arr != NULL)
+					delete[] arr;
+				arr = new T[_size];
+				for (int i = 0; i < _size; i++)
+					this->arr[i] = other.arr[i];
+			}
+			return *this;
+		}
+		~Array()
+		{
+			delete[] arr;
+		}
 		unsigned int size() const;
 		class outOfBounds: public std::exception
 		{
 			public:
-				const char *what() const throw();
-		}
+				const char *what() const throw()
+				{
+					return "Index is out of bounds!";
+				}
+		};
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,20 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 16:48:55 by jetan             #+#    #+#             */
-/*   Updated: 2025/05/26 16:48:56 by jetan            ###   ########.fr       */
+/*   Updated: 2025/05/27 14:49:46 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+int main()
+{
+	//Creates an empty array
+	Array<int> empty;
+	//Creates an array of n elements initialized by default
+	int * a = new int();
+	std::cout << *a << std::endl;
+	//deep copy
+	int original[] = {1, 2, 3};
+	std::cout << original[0] << std::endl;
+}
